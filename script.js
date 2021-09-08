0287d8adbd542da0852c24ce3cb3ec865b14dbe4
parkyounglak <kpgakiller@gmail.com>
2021-09-08 12:47:43 +0900

    0908

---
+++ script.js
@@ -0,0 +1,62 @@
+const tdList = document.querySelectorAll("#calendarTable td");
+//td에 마우스를 올리면 .schedule 의 상태를 보이게 바꾼다. mouseover
+//마우스가 td에서 나가면 .schedule 을 보이지 않게 한다.  mouseleave  
+// 상기 2개를 합치면 hover 는 js에서 구현 X.
+//onmouseover/mouseout : 자식 요소에 이벤트를 전달.
+//mouseenter : 이벤트 전달을 하지 않는다.
+//화살표 함수는 this 를 최상위 부모인 window로 받는다. (블럭을 무시)
+console.log(tdList)
+  tdList.forEach((td)=>{
+
+  })
+  for(td of tdList){
+    td.onmouseenter  = function(event){
+      event.target.querySelector(".schedule").style.visibility="visible";
+    }
+    td.addEventListener("mouseleave",function(event){
+    event.target.querySelector(".schedule").style.visibility="hidden";
+  })
+  }  
+  
+  //9월의 마지막일 
+  
+
+  //날짜뿌리기
+  function DateBringer(){
+  for(let i=firstDay,d=1;i<lastDay+firstDay;i++){
+    tdList[i].querySelector(".day").innerText= d++;
+  };
+  }
+  
+let toDay = new Date();
+//let date = new Date();
+let thisMonth = toDay.getMonth();
+let lastDay = new Date(2021,9,0).getDate();
+  //9월의 시작하는 요일 
+  let firstDay = new Date(2021,8,1).getDay();
+  //let a=1,b=2 이런 식으로 선언 가능.
+
+  function prevMonth(){
+    console.log("!!") 
+    //DateBringer()
+    
+    let lastDay = new Date(2021,thisMonth,0).getDate();
+    let firstDay = new Date(2021,thisMonth-1).getDay();
+    for(let i=firstDay,d=1;i<lastDay+firstDay;i++){
+    tdList[i].querySelector(".day").innerText= d++;
+  };
+    console.log("##");
+  }
+  function nextMonth(){
+    console.log("!!")
+    //DateBringer
+    let lastDay = new Date(2021,thisMonth+1,0).getDate();
+    let firstDay = new Date(2021,thisMonth+1).getDay();
+    for(let i=firstDay,d=1;i<lastDay+firstDay;i++){
+    tdList[i].querySelector(".day").innerText= d++;
+  };
+     console.log("##");
+  }
+  
+
+  DateBringer()
