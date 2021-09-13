199f5afeff29bdae7949ac193bf414ce3eb78efd
parkyounglak <kpgakiller@gmail.com>
2021-09-13 10:09:43 +0900

    0913

--- script/memo.js
+++ script/memo.js
@@ -1,5 +1,9 @@
 //데이터베이스 서버와 통신해서 9/10 일 할일 목록을 받았다.
 const d_09_10=["aaa","bbb","ccc","ddd"];
+const displaySection = document.getElementById('dp'); //입력된 메세지가 표시되는곳
+const inputMessage = document.getElementById('inputHere'); //입력받는곳
+const inputBtn = document.querySelector("button");
+const delBtn = document.getElementsByClassName("delete");
 schedulePrint(); //호이스팅
 
 // 스케줄 추가버튼을 누르면 데이터베이스 서버에 insert를 하고
@@ -9,7 +13,7 @@ function addScedule(){
 
 }
 //리스트에 삭제 버튼을 누르면 데이터베이스 서버에서 삭제하고 다시 리스트를 받는다.
-//기존의 리스트를 삭제하는 것으로 대신.
+//기존의 리스트를 삭제하는 것으로 대신..
 
 
 
